243c8da388903119309a03dc33aa106cdd2b6099
Osama Ilyas <IlyasO@cardiff.ac.uk>
2023-02-08 15:57:07 +0000

    Made a very basic login form, will need to come to this at a later point to update it.

---
+++ CalorieCounterWeb/UserInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CalorieCounterWeb
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(50, ErrorMessage = "Name is too long, enter a new name")]
+        public string? Name { get; set; }
+
+        [Required, StringLength(50, ErrorMessage = "Username is too long, enter a new username")]
+        public string? UserName { get; set; }
+
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+
+        [Required, StringLength(100, ErrorMessage = "Password is too long, enter a new password"), PasswordPropertyText]
+        public string? Password { get; set; }
+
+        [MaxLength(10)]
+        public int Age { get; set; }
+
+    }
+}
